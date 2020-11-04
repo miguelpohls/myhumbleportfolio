90db01a450e75e81a888035fbf58183de76131ae
miguelpohls <65691724+miguelpohls@users.noreply.github.com>
2020-11-03 19:33:31 -0600

    Add files via upload

---
+++ simple projects/stalker.js
@@ -0,0 +1,8 @@
+var userFirstName = prompt("What is your first name?");
+var userLastName = prompt("What is your last name?");
+var userAge = prompt("What is your Age?");
+
+var fullName = userFirstName + " " + userLastName;
+
+console.log("Your full name is " + fullName);
+console.log("Your are " + userAge + " years old");
