60e0876013410463a5f0bed86172f69a80e80871
miguelpohls <65691724+miguelpohls@users.noreply.github.com>
2020-11-03 19:29:22 -0600

    Add files via upload

---
+++ jquery/jqueryexercise.js
@@ -0,0 +1,9 @@
+$("div").css("background","purple");
+
+$(".highlight").css("width", "200px");
+
+$("#third").css("border", "2px solid orange");
+
+/*$("div:first").css("color", "pink")*/
+
+$("div:first-of-type").css("color", "pink");
